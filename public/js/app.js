7f0550e51979fb0b5c88f11354ff80086282e369
Gary Day <garyday@hotmail.co.uk>
2016-04-25 15:44:07 +0100

    Added name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,22 @@
 var socket = io();
+var room = getQueryVariable('room');
+var name = getQueryVariable('name') || 'Guest';
 
 socket.on('connect', function(){
 	console.log('Connected to socket.io server!');
+	jQuery('.messages').append('<p><strong>' + name + ' joined ' + room + '</strong></p>');
 });
 
 socket.on('message', function(message){
 	
 	var timestampMoment = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
 
 	console.log('New message:');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>' + timestampMoment.local().format('h:mma') + ': </strong>' + message.text + '</p>');
+	$message.append('<p><strong>' + message.name + ' ' + timestampMoment.local().format('h:mma') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting of new message
@@ -23,6 +28,7 @@ $form.on('submit', function(event){
 	var $message = $form.find('input[name=message]')
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 
