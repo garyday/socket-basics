b5cc90a00a6a29a0375a39452e3b43b77651a606
Gary Day <garyday@hotmail.co.uk>
2016-04-25 15:03:39 +0100

    show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,13 @@ socket.on('connect', function(){
 });
 
 socket.on('message', function(message){
+	
+	var timestampMoment = moment.utc(message.timestamp);
+
 	console.log('New message:');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p>' + message.text + '</p>');
+	jQuery('.messages').append('<p><strong>' + timestampMoment.local().format('h:mma') + ': </strong>' + message.text + '</p>');
 });
 
 // Handles submitting of new message
